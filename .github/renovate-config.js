c0bba324487dae4a221cb29257aa6fee331f3f5b
Julian Schallenmueller <jsa@novatec-gmbh.de>
2025-02-18 16:56:49 +0100

    Configuration...

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,3 +1,26 @@
 module.exports = {
-  extends: ["config:recommended", ":dependencyDashboard"]
-}
+  extends: ["config:recommended", ":dependencyDashboard"],
+  branchPrefix: 'renovate/',
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  forkProcessing: 'enabled',
+  dryRun: 'full',
+  repositories: ['JulianSchallenmueller/azure-static-webapp-poc'],
+  packageRules: [
+    {
+      description: 'lockFileMaintenance',
+      matchUpdateTypes: [
+        'pin',
+        'digest',
+        'patch',
+        'minor',
+        'major',
+        'lockFileMaintenance',
+      ],
+      dependencyDashboardApproval: false,
+      minimumReleaseAge: '0 days',
+    },
+  ],
+};
